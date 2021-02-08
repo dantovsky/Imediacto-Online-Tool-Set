5fafd80d008728e6d783ab05485b87c81c716b3f
Dante Marinho <dantecnia@hotmail.com>
2021-02-08 03:56:32 +0000

    Developing diff arrays tool.

--- tools/shared/js/scripts.js
+++ tools/shared/js/scripts.js
@@ -1,25 +1,54 @@
 // Global functions for tools
 // ---------------------------------------------
 function copyToClipboard(idElement = "output") {
-  let copyText = document.getElementById(idElement);
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
+    let element = document.getElementById(idElement);
+    const typeElement = element.tagName.toLowerCase()
 
-  let check = document.getElementById("check");
-  check.classList.remove("none");
-  setTimeout(() => {
-    check.classList.add("none");
-  }, 3000);
+    if (typeElement === 'textarea') {
+        selectAndCopy(element)
+    } else if (typeElement === 'div') {
+        const newTextarea = document.createElement('textarea')
+        newTextarea.value = document.getElementById(idElement).innerHTML
+        document.body.appendChild(newTextarea)
+        selectAndCopy(newTextarea)
+        newTextarea.remove()
+    }
+
+    let check = document.getElementById("check");
+    check.classList.remove("none");
+    setTimeout(() => {
+        check.classList.add("none");
+    }, 3000);
 }
+
+function selectAndCopy(element) {
+    element.select();
+    element.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
+
+
+
+// function copyToClipboardCOPY(idElement = "output") {
+//     let copyText = document.getElementById(idElement);
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999);
+//     document.execCommand("copy");
+
+//     let check = document.getElementById("check");
+//     check.classList.remove("none");
+//     setTimeout(() => {
+//         check.classList.add("none");
+//     }, 3000);
+// }
 
 // Word count
 function wordCount(idElement, maxLength = 100000) {
-  var maxLength = maxLength;
-  jQuery("#max-length-chars").text(maxLength);
-  jQuery('textarea#' + idElement).attr("maxlength", maxLength);
-  jQuery('textarea#' + idElement).keyup(function () {
-    var textlen = jQuery(this).val().length;
-    jQuery("#counting-chars").text(textlen);
-  });
+    var maxLength = maxLength;
+    jQuery("#max-length-chars").text(maxLength);
+    jQuery('textarea#' + idElement).attr("maxlength", maxLength);
+    jQuery('textarea#' + idElement).keyup(function () {
+        var textlen = jQuery(this).val().length;
+        jQuery("#counting-chars").text(textlen);
+    });
 }
